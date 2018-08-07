81364b3028a81e86c9c9b1b5186c2d6e117f8c1c
PolGuixe <pol.guixe@gmail.com>
2018-08-07 19:08:21 +0200

    updated package.js

--- package.js
+++ package.js
@@ -1,21 +1,18 @@
 Package.describe({
   name: "origenstudio:vulcan-material-ui-layout",
   summary: "Material UI theme for Vulcan",
-  version: '0.0.1',
-  git: "", // coming soon
+  version: "0.0.1",
+  git: "https://github.com/OrigenStudio/vulcan-material-ui-layout"
 });
 
-Package.onUse(function (api) {
-
-  api.versionsFrom(['METEOR@1.0']);
+Package.onUse(function(api) {
+  api.versionsFrom(["METEOR@1.0"]);
 
   api.use([
     // Vulcan packages
-    'vulcan:core@1.8.11',
-    'zetoff:vulcan',
+    "vulcan:core@1.8.11"
   ]);
 
   api.mainModule("lib/client.js", "client");
   api.mainModule("lib/server.js", "server");
-
 });
